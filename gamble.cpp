929284ed9488b23a1da44634dfe55d5d48b7477e
Robo-Hermano <138218435+Robo-Hermano@users.noreply.github.com>
2024-09-28 11:37:25 +0100

    Create gamble.cpp

---
+++ gamble.cpp
@@ -0,0 +1,26 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <random>
+#include <unordered_map>
+#include <list>
+#include <random>
+
+using namespace std;
+
+void loop() {
+    unordered_map <string, int> CardValues;
+    list<string> CardTypes = {"Kalp", "Elmas", "Maça", "Sinek"};
+    CardValues["ikili"] = 2;
+    CardValues["üçlü"] = 3;
+    CardValues["dörtlü"] = 4;
+    
+    
+}
+
+int main() {
+    cout << "Blackjack'e hoşgeldin aga" << endl;
+    cout << "Hocam kumarda çok para diğe birşey yok";
+    loop();
+    return 0;
+}
+
